bb2fc412ac990979a7ac6c3d9b4d6c295907d74d
Robert Arifulin <robertarif2015@gmail.com>
2022-03-29 17:01:46 +0300

    changes 2

--- website/script.js
+++ website/script.js
@@ -5,4 +5,8 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 function main() {
     let value = params.code; // 
     console.log(value);
+    let a1 = document.getElementById("a1");
+    let a2 = document.getElementById("a2");
+    a1.href = "robert://" + value;
+    a2.href = "exp://172.20.10.4:19000/--/" + value
 }  
