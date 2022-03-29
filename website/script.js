9047ccc427ce357d189e6a35a32c896b5c2b3f08
Robert Arifulin <robertarif2015@gmail.com>
2022-03-29 16:51:42 +0300

    changes 1

--- website/script.js
+++ website/script.js
@@ -2,5 +2,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-  let value = params.code; // 
-  console.log(value)
+function main() {
+    let value = params.code; // 
+    console.log(value);
+}  
