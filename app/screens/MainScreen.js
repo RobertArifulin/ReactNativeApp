9b8e77ddfa033b23a84de4156e3f6ea7271c2233
Robert Arifulin <robertarif2015@gmail.com>
2022-03-29 17:23:38 +0300

    changes 3

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -6,34 +6,39 @@ import Kard from '../modules/Kard';
 
 const {screenWidth, screenHeight} = Dimensions.get("screen");
 
-function MainScreen(props) {
-    const useMount = func => useEffect(() => func(), []);
-    const useInitialURL = () => {
-        const [url, setUrl] = useState(null);
-        const [processing, setProcessing] = useState(true);
-    
-        useMount(() => {
-        const getUrlAsync = async () => {
-            // Get the deep link used to open the app
-            const initialUrl = await Linking.getInitialURL();
-            console.log(initialUrl);
-            // The setTimeout is just for testing purpose
-            setTimeout(() => {
-            setUrl(initialUrl);
-            setProcessing(false);
-            }, 1000);
-        };
-    
-        getUrlAsync();
-        });
-    
-        return { url, processing };
+const useMount = func => useEffect(() => func(), []);
+const useInitialURL = () => {
+    const [url, setUrl] = useState(null);
+    const [processing, setProcessing] = useState(true);
+
+    useMount(() => {
+    const getUrlAsync = async () => {
+        // Get the deep link used to open the app
+        const initialUrl = await Linking.getInitialURL();
+        console.log(initialUrl);
+        // The setTimeout is just for testing purpose
+        setTimeout(() => {
+        setUrl(initialUrl);
+        setProcessing(false);
+        }, 1000);
     };
+
+    getUrlAsync();
+    });
+
+    return { url, processing };
+};
+
+function MainScreen(props) {
+    const { url: initialUrl, processing } = useInitialURL();
+    console.log(1);
+    console.log(initialUrl);
+
     const redirect_uri = "https://robertarifulin.github.io/ReactNativeApp/";
    
     const URL = 'https://api.notion.com/v1/oauth/authorize?owner=user&client_id=728d9ca7-3680-4bfd-a63f-adacfa7ca050&redirect_uri=' + redirect_uri + '&response_type=code';
-    // https://api.notion.com/v1/oauth/authorize?owner=user&client_id=728d9ca7-3680-4bfd-a63f-adacfa7ca050&redirect_uri=https://robertarifulin.github.io/ReactNativeApp/&response_type=code
-    // https://www.notion.so/oauth2callback?state=eyJjYWxsYmFja1R5cGUiOiJyZWRpcmVjdCIsImVuY3J5cHRlZFRva2VuIjoiZjA4OGQ2YzFkMDg3ZDU5NmUwMGFkOWM2YjU2NTQyNzAyMDgwZDdjOWE0NWI0MmMwYjE3NDdhYWVjMTQ0MWI2NjhjOTgzNTA0YzQ5NjVlOTg1MWNkYWYzYTQwYTI5ZGI1YzgzN2NmZWQ5YjAwNGI2NjcwZDkwMTI2ODFiOTBmZjgyODNmNTI1NWE4MzgwMDMzZDgyYjRhMzU2ZjU4In0%3D&code=4%2F0AX4XfWienu9jDH9RSWJzZtCiAe-wKtkvwiNl5sIiNvUDBabUgvF5PA0FpN42xkHAFp09QA&scope=email%20profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20openid&authuser=0&prompt=none
+    // https://robertarifulin.github.io/ReactNativeApp/?code=2e595715-68a2-454a-a857-510e4baaf687&state=
+    // https://www.notion.so/oauth2callback?s   tate=eyJjYWxsYmFja1R5cGUiOiJyZWRpcmVjdCIsImVuY3J5cHRlZFRva2VuIjoiZjA4OGQ2YzFkMDg3ZDU5NmUwMGFkOWM2YjU2NTQyNzAyMDgwZDdjOWE0NWI0MmMwYjE3NDdhYWVjMTQ0MWI2NjhjOTgzNTA0YzQ5NjVlOTg1MWNkYWYzYTQwYTI5ZGI1YzgzN2NmZWQ5YjAwNGI2NjcwZDkwMTI2ODFiOTBmZjgyODNmNTI1NWE4MzgwMDMzZDgyYjRhMzU2ZjU4In0%3D&code=4%2F0AX4XfWienu9jDH9RSWJzZtCiAe-wKtkvwiNl5sIiNvUDBabUgvF5PA0FpN42xkHAFp09QA&scope=email%20profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20openid&authuser=0&prompt=none
     function hyperLink(){
         Linking.openURL(URL);
     }
--- website/script.js
+++ website/script.js
@@ -2,11 +2,17 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let value = "";
 function main() {
-    let value = params.code; // 
+    value = params.code; // 
     console.log(value);
-    let a1 = document.getElementById("a1");
-    let a2 = document.getElementById("a2");
-    a1.href = "robert://" + value;
-    a2.href = "exp://172.20.10.4:19000/--/" + value
-}  
+    document.getElementById("a1").onclick = function() {
+        document.getElementById("a1").href = "robert://" + value;
+        return false;
+    };
+    document.getElementById("a2").onclick = function() {
+        document.getElementById("a2").href ="exp://172.20.10.4:19000/--/" + value;
+        return false;
+    };
+    console.log(document.getElementById("a2").href);
+}
